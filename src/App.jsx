03ff9a0c8a7c179b9b3c5f2c60b9dbda901311a3
Chanawin-kmpn <pond_chanawin@hotmail.com>
2025-06-19 09:47:02 +0700

    add project to repository

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<main className="text-5xl font-bold">
+			<h1 className="text-violet-300">Welcome to Awwwards</h1>
+		</main>
+	);
+};
+
+export default App;
