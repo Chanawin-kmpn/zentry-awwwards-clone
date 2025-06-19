582086fe66f4abe740cc135a49289260bb39da94
Chanawin-kmpn <pond_chanawin@hotmail.com>
2025-06-19 13:31:54 +0700

    add GSAP and React Icons dependencies; implement Hero and Button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
 	return (
-		<main className="text-5xl font-bold">
-			<h1 className="text-violet-300">Welcome to Awwwards</h1>
+		<main className="relative min-h-screen w-screen overflow-x-hidden">
+			<Hero />
+			<About />
 		</main>
 	);
 };
